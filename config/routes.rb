1c9925fd9b30e63da211cab6d6a19a951a54a293
vagrant <vagrant@localhost.localdomain>
2020-11-02 05:36:31 +0000

    edit-user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  resources :posts, :only => [:new, :create, :show, :destory]
-  resources :users, :only => [:show, :edit, :update, :destory]
+  resources :posts, :only => [:new, :create, :show, :destroy]
+  resources :users, :only => [:show, :edit, :update, :destroy]
 end
