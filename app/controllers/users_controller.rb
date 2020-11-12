9e02e2c244757c85afc5fedc6dd4bb36c41002dc
vagrant <vagrant@localhost.localdomain>
2020-11-12 06:58:15 +0000

    [add]login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,15 @@ class UsersController < ApplicationController
   	@posts = @user.posts
   end
 
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
   def edit
   	@user = User.find(params[:id])
   end
